5258d222416d7fa33fefbb0b7d4e02411b150873
Adam Hannigan <adamhannigan81@gmail.com>
2024-09-04 16:18:19 +1000

    Fulfilling

--- src/features/game/lib/gameMachine.ts
+++ src/features/game/lib/gameMachine.ts
@@ -1515,14 +1515,14 @@ export function startGame(authContext: AuthContext) {
                 });
               }
 
-              const state = await postEffect({
+              const { gameState, data } = await postEffect({
                 farmId: Number(context.farmId),
                 effect,
                 token: authContext.user.rawToken as string,
                 transactionId: context.transactionId as string,
               });
 
-              return { state };
+              return { state: gameState, response: data };
             },
             onDone: [
               {
